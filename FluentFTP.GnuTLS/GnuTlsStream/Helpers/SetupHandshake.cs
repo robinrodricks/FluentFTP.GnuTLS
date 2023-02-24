1a89be00f7038395bb3fe21a20ed5751582182c9
FanDjango <FanDjango@ansynova.eu>
2023-02-24 19:50:03 +0100

    comment

--- FluentFTP.GnuTLS/GnuTlsStream/Helpers/SetupHandshake.cs
+++ FluentFTP.GnuTLS/GnuTlsStream/Helpers/SetupHandshake.cs
@@ -94,5 +94,35 @@ namespace FluentFTP.GnuTLS {
 			return true;
 		}
 
+		// An alternative would be (c/o MSDN)
+		// This is how you can determine whether a socket is still connected:
+
+		//bool blockingState = client.Blocking;
+
+		//try
+		//{
+		//    byte[] tmp = new byte[1];
+
+		//	client.Blocking = false;
+		//	client.Send(tmp, 0, 0);
+		//    Console.WriteLine("Connected!");
+		//}
+		//catch (SocketException e)
+		//{
+		//    // 10035 == WSAEWOULDBLOCK
+		//    if (e.NativeErrorCode.Equals(10035))
+		//    {
+		//		Console.WriteLine("Still Connected, but the Send would block");
+		//	}
+		//	else
+		//	{
+		//		Console.WriteLine("Disconnected: error code {0}!", e.NativeErrorCode);
+		//	}
+		//}
+		//finally
+		//{
+		//	client.Blocking = blockingState;
+		//}
+
 	}
 }
