3053b9a6c5d0b927110459d531852945d851cfb0
FanDjango <51046875+FanDjango@users.noreply.github.com>
2024-03-29 16:35:56 +0100

    New message category Read/Write Control/Data

--- FluentFTP.GnuTLS/Enums/GnuMessage.cs
+++ FluentFTP.GnuTLS/Enums/GnuMessage.cs
@@ -13,15 +13,17 @@ namespace FluentFTP.GnuTLS.Enums {
 		FunctionLoader = 1 << 2,
 		Handshake = 1 << 3,
 		Alert = 1 << 4,
-		Read = 1 << 5,
-		Write = 1 << 6,
-		ClientCertificateValidation = 1 << 7,
-		ShowClientCertificateInfo = 1 << 8,
-		ShowClientCertificatePEM = 1 << 9,
-		X509 = 1 << 10,
-		RAWPK = 1 << 11,
+		ReadControl = 1 << 5,
+		ReadData = 1 << 6,
+		WriteControl = 1 << 7,
+		WriteData = 1 << 8,
+		ClientCertificateValidation = 1 << 9,
+		ShowClientCertificateInfo = 1 << 10,
+		ShowClientCertificatePEM = 1 << 11,
+		X509 = 1 << 12,
+		RAWPK = 1 << 13,
 
-		All = unchecked((ushort)-1 - Read - Write),
+		All = unchecked((ushort)-1 - ReadControl - ReadData - WriteControl - WriteData),
 	}
 
 }
